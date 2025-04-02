e26ffff1e3f4149fa5d14a84e6b8e4584a78c929
Aryan <Aryantech03@gmail.com>
2025-04-02 20:34:48 +0530

    -

--- Project/REACT_Project/TODO_MERN_APP/frontend/src/App.js
+++ Project/REACT_Project/TODO_MERN_APP/frontend/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import Footer from './components/footer/Footer';
-
+import About from './components/about/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+      
+      {/* <About /> */}
       <Footer />
     </div>
   );
---
+++ Project/REACT_Project/TODO_MERN_APP/frontend/src/components/about/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="about d-flex justify-content-center align-items-center">
+      <div className="container">
+        <div className="d-flex">
+          <h1>About Us</h1>
+        </div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem tenetur,
+          aliquam expedita illum eius provident exercitationem ipsa illo!
+          Dolorum reiciendis nihil itaque pariatur repellat velit rem quia ipsa?
+          Nesciunt, nobis. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Veritatis consequuntur doloremque perferendis esse itaque
+          nesciunt rem et? Culpa quis optio impedit vero minus explicabo
+          doloremque, corporis atque pariatur in praesentium. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. At quo, placeat beatae eaque
+          itaque nemo tenetur modi libero quia in ad vitae. Ratione odit
+          consectetur quaerat voluptatem quia dolores dicta? <br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
+          odio, quam voluptates voluptate necessitatibus cum pariatur illo
+          veritatis commodi reprehenderit? Harum doloribus deleniti earum
+          perspiciatis aliquam, molestiae odio perferendis? Dicta. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Necessitatibus quis autem
+          hic ipsam adipisci sed eligendi, minima vel quisquam, laboriosam
+          deleniti dicta quasi sapiente quos accusantium cumque tempora
+          dignissimos natus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
