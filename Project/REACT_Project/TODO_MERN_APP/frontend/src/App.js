275986e8ebbee5822eaa57fc29dbd89fd724a2d6
Aryan <Aryantech03@gmail.com>
2025-04-03 20:52:32 +0530

    -

--- Project/REACT_Project/TODO_MERN_APP/frontend/src/App.js
+++ Project/REACT_Project/TODO_MERN_APP/frontend/src/App.js
@@ -6,6 +6,7 @@ import About from './components/about/About';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from './components/signup/Signup';
 import Signin from './components/signup/Signin';
+import Todo from './components/todo/Todo';
 const App = () => {
   return (
     <div>
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/todo" element={<Todo />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
         </Routes>
---
+++ Project/REACT_Project/TODO_MERN_APP/frontend/src/components/todo/Todo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./todo.css";
+
+const Todo = () => {
+  return (
+    <div className="todo">
+      <div className="todo-main container d-flex justify-content-center align-items-center">
+        <div className="d-flex flex-column todo-inputs-div w-25">
+          <input
+            type="text"
+            placeholder="TITLE"
+            className="my-2 p-2 todo-inputs"
+          />
+          <textarea
+            type="text"
+            placeholder="BODY"
+            className="p-2 todo-inputs"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
