509fff6215230fd678be9b0894cd5d87531ff0f8
Aryan <Aryantech03@gmail.com>
2025-04-03 23:40:10 +0530

    -

--- Project/REACT_Project/TODO_MERN_APP/frontend/src/components/todo/Todo.jsx
+++ Project/REACT_Project/TODO_MERN_APP/frontend/src/components/todo/Todo.jsx
@@ -2,20 +2,28 @@ import React from "react";
 import "./todo.css";
 
 const Todo = () => {
+  const show = () => {
+    document.getElementById("textarea").style.display = "block";
+  }
   return (
     <div className="todo">
-      <div className="todo-main container d-flex justify-content-center align-items-center">
-        <div className="d-flex flex-column todo-inputs-div w-25">
+      <div className="todo-main container d-flex justify-content-center align-items-center my-4 flex-column">
+        <div className="d-flex flex-column todo-inputs-div w-50 p-1">
           <input
             type="text"
             placeholder="TITLE"
             className="my-2 p-2 todo-inputs"
+            onClick={show}
           />
           <textarea
+            id="textarea"
             type="text"
             placeholder="BODY"
             className="p-2 todo-inputs"
           />
+        </div>
+        <div className="w-50 d-flex justify-content-end my-3">
+          <button className="home-btn px-2 py-1">Add</button>
         </div>
       </div>
     </div>
