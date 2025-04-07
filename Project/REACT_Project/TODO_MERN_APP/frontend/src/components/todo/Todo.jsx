268a0e9db50195b4ee9ca8362ea800ccf9a5cf0a
Aryan <Aryantech03@gmail.com>
2025-04-07 09:24:28 +0530

    -

--- Project/REACT_Project/TODO_MERN_APP/frontend/src/components/todo/Todo.jsx
+++ Project/REACT_Project/TODO_MERN_APP/frontend/src/components/todo/Todo.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./todo.css";
+import TodoCards from "./TodoCards";
 
 const Todo = () => {
+  const [Inputs, setInputs] = useState({ title: "", body: "" });
+  const [Array, setArray] = useState([]);
   const show = () => {
     document.getElementById("textarea").style.display = "block";
-  }
+  };
+  const change = (e) => {
+    const { name, value } = e.target;
+    setInputs({ ...Inputs, [name]: value });
+  };
+  const submit = () => {
+    setArray([...Array, Inputs]);
+    setInputs({ title: "", body: "" });
+  };
   return (
     <div className="todo">
       <div className="todo-main container d-flex justify-content-center align-items-center my-4 flex-column">
@@ -14,16 +25,36 @@ const Todo = () => {
             placeholder="TITLE"
             className="my-2 p-2 todo-inputs"
             onClick={show}
+            name="title"
+            value={Inputs.title}
+            onChange={change}
           />
           <textarea
             id="textarea"
             type="text"
             placeholder="BODY"
+            name="body"
             className="p-2 todo-inputs"
+            value={Inputs.body}
+            onChange={change}
           />
         </div>
         <div className="w-50 d-flex justify-content-end my-3">
-          <button className="home-btn px-2 py-1">Add</button>
+          <button className="home-btn px-2 py-1" onClick={submit}>
+            Add
+          </button>
+        </div>
+      </div>
+      <div className="todo-body">
+        <div className="container-fluid">
+          <div className="row">
+            {Array &&
+              Array.map((item, index) => (
+                <div className="col-lg-3 mx-5 my-2">
+                  <TodoCards />
+                </div>
+              ))}  
+          </div>
         </div>
       </div>
     </div>
---
+++ Project/REACT_Project/TODO_MERN_APP/frontend/src/components/todo/TodoCards.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TodoCards = () => {
+  return (
+    <div className="p-3 todo-card">
+        <div>
+            <h5>Heading</h5>
+            <p className="todo-card-p">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi corporis voluptatibus ipsum dolore quas necessitatibus neque voluptates tenetur ad perspiciatis. Nam facere, rerum quibusdam assumenda incidunt veritatis eligendi cum quo?
+            </p>
+        </div>
+    </div>
+  );
+};
+
+export default TodoCards;
