eb230e9c1c9602dda5c88b8335f6017a1da16818
Aryan <Aryantech03@gmail.com>
2025-06-18 09:36:15 +0530

    -

---
+++ Project/HTML_CSS_JS_Project/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".hire-me").addEventListener("click", function () {
+  alert("You clicked Hire Me!");
+});
+
+document.querySelector(".chat").addEventListener("click", function () {
+  alert("You clicked Let's Chat!");
+});
