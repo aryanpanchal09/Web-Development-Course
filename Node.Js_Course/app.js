1fc0fc56d6a9c268d16976571ce192d314877bd3
Aryan <Aryantech03@gmail.com>
2025-07-09 19:23:55 +0530

    -

--- Node.Js_Course/app.js
+++ Node.Js_Course/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
@@ -17,7 +19,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  /* res.status(404).send('<h1>Page not found</h1>'); */
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 /*
--- Node.Js_Course/routes/shop.js
+++ Node.Js_Course/routes/shop.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   /* console.log('In another middleware'); */
-  res.send('<h1>Hello from express!</h1>');
+  /* res.send('<h1>Hello from express!</h1>'); */
+  /* res.sendFile('/views/shop.html'); -> will not work as it requires absolute path */
+  res.sendFile(
+    path.join(rootDir, 'views', 'shop.html')
+  ); /* it detects the operating system  */
 });
 
 module.exports = router;
