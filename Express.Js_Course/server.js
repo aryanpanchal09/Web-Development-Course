e6370df72e633a6fd8aa302c8476e638ae3aedc6
Aryan <Aryantech03@gmail.com>
2025-07-10 08:01:02 +0530

    -

--- Express.Js_Course/server.js
+++ Express.Js_Course/server.js
@@ -1,15 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  console.log("Here in Terminal");
+app.get('/', (req, res) => {
+  console.log('Here in Terminal');
   /* res.send("Hii in the browser"); */
   /* res.status(500).send("Hi in browser and 500 status code in console"); */
   /* res.json({ message: "json" }); */
   /* res.download("server.js"); */
-  res.render("index", { text: "World" });
+  res.render('index', { text01: 'World' });
 });
+
+const userRouter = require('./routes/users.js');
+
+app.use('/users', userRouter);
+
+/* app.get('/users', (req, res) => {
+  console.log('Users');
+  res.send('User List');
+});
+
+app.get('/users/new', (req, res) => {
+  res.send('Users New Form');
+}); */
 
 app.listen(3000);
